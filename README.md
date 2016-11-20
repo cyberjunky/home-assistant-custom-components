41f76cc7f25210f2035c5c6978da3af5f78ef257
Ron Klinkien <ron@home-logger>
2016-11-20 17:04:47 +0100

    New components toon_smartmeter and solarportal.

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ You can also control the thermostat Mode and Setpoint (target temperature)
 
 Copy file climate/toon.py to your ha_config_dir/custom-components/climate directory.
 
-Configuration:
+Example of the Configuration:
 
 ```
 configuration.yaml
@@ -33,3 +33,121 @@ climate:
 ### Screenshot
 
 ![alt text](https://raw.githubusercontent.com/cyberjunky/home-assistant-custom-components/master/screenshots/toon.png "Screenshot")
+
+
+## Toon Smart Meter component
+
+NOTE: This component only works with rooted Toon devices.
+
+It reads Smart Meter data from your Toon, gathered by the meteradapter.
+
+### Install
+
+Copy file sensor/toon_smartmeter.py to your ha_config_dir/custom-components/sensor directory.
+
+Example of the Configuration:
+
+```
+configuration.yaml
+
+sensor:
+  - platform: toon_smartmeter
+    host: <IP address of your Toon>
+    port: <Port used by your Toon> (default is 10080)
+    scan_interval: 10
+    resources:
+      - gasused
+      - gasusedcnt
+      - elecusageflowlow
+      - elecusagecntlow
+      - elecusageflowhigh
+      - elecusagecnthigh
+      - elecprodflowlow
+      - elecprodcntlow
+      - elecprodflowhigh
+      - elecprodcnthigh
+```
+
+The resources section tells the component which values to gather and display, you can leave out the prod values if your don't generate power.
+
+![alt text](https://raw.githubusercontent.com/cyberjunky/home-assistant-custom-components/master/screenshots/toon-smartmeter-badges.png "Toon SmartMeter Badges")
+
+If you want them grouped instead of having the separate sensor badges, you can use this in your groups.yaml:
+
+```
+Smart meter:
+  - sensor.p1_gas_used_last_hour
+  - sensor.p1_gas_used_cnt
+  - sensor.p1_power_prod_low
+  - sensor.p1_power_prod_high
+  - sensor.p1_power_prod_cnt_low
+  - sensor.p1_power_prod_cnt_high
+  - sensor.p1_power_use_cnt_low
+  - sensor.p1_power_use_cnt_high
+  - sensor.p1_power_use_low
+  - sensor.p1_power_use_high
+```
+
+### Screenshot
+
+![alt text](https://raw.githubusercontent.com/cyberjunky/home-assistant-custom-components/master/screenshots/toon-smartmeter.png "Screenshot Toon SmartMeter")
+
+
+## SolarPortal component
+
+There are several solarpower portals storing you power generation data using the same api. So you can query the information uploaded by your solarpanels.
+I have a Omnik inverted and so I'm using it with omnikportal
+
+### Install
+
+Copy file sensor/solarportal.py to your ha_config_dir/custom-components/sensor directory.
+
+Example of the Configuration:
+
+```
+configuration.yaml
+
+sensor:
+  - platform: solarportal
+    host: www.omnikportal.com
+    port: 10000
+    username: <Your portal username>
+    password: <Your portal password>
+    scan_interval: 30
+    resources:
+      - actualpower
+      - energytoday
+      - energytotal
+      - incometoday
+      - incometotal
+```
+
+Supported portals for host entry:
+   www.omnikportal.com
+   www.ginlongmonitoring.com
+   log.trannergy.com
+   www.solarmanpv.com
+
+
+The resources section tells the component which values to display.
+
+![alt text](https://raw.githubusercontent.com/cyberjunky/home-assistant-custom-components/master/screenshots/solarportal-badges.png "SolarPortal Badges")
+
+If you want them grouped instead of having the separate sensor badges, you can use this in your groups.yaml:
+
+```
+OmnikPortal Solar:
+  - sensor.solar_actual_power
+  - sensor.solar_energy_today
+  - sensor.solar_energy_total
+  - sensor.solar_income_today
+  - sensor.solar_income_total
+```
+
+![alt text](https://raw.githubusercontent.com/cyberjunky/home-assistant-custom-components/master/screenshots/solarportal.png "Screenshot SolarPortal")
+
+### Some graphs
+
+![alt text](https://raw.githubusercontent.com/cyberjunky/home-assistant-custom-components/master/screenshots/solarportal-graph.png "Graph Actual Power")
+![alt text](https://raw.githubusercontent.com/cyberjunky/home-assistant-custom-components/master/screenshots/solarportal-graph-income.png "Graph Total Income")
+
---
+++ sensor/toon_smartmeter.py
@@ -0,0 +1,175 @@
+"""
+Support for reading SmartMeter data through Eneco's Toon thermostats.
+Only works for rooted Toon.
+
+configuration.yaml
+
+sensor:
+  - platform: toon_smartmeter
+    host: ip_address
+    port: 10080
+    scan_interval: 10
+    resources:
+      - gasused
+      - gasusedcnt
+      - elecusageflowlow
+      - elecusagecntlow
+      - elecusageflowhigh
+      - elecusagecnthigh
+      - elecprodflowlow
+      - elecprodcntlow
+      - elecprodflowhigh
+      - elecprodcnthigh
+"""
+import logging
+import voluptuous as vol
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import (
+    CONF_HOST, CONF_PORT, CONF_SCAN_INTERVAL, CONF_RESOURCES)
+from homeassistant.helpers.entity import Entity
+
+from urllib.request import urlopen
+import json
+
+_LOGGER = logging.getLogger(__name__)
+
+BASE_URL = 'http://{0}:{1}{2}'
+SENSOR_PREFIX = 'P1 '
+
+SENSOR_TYPES = {
+    'gasused': ['Gas Used Last Hour', 'm3', 'mdi:fire'],
+    'gasusedcnt': ['Gas Used Cnt', 'm3', 'mdi:fire'],
+    'elecusageflowlow': ['Power Use Low', 'Watt', 'mdi:flash'],
+    'elecusageflowhigh': ['Power Use High', 'Watt', 'mdi:flash'],
+    'elecprodflowlow': ['Power Prod Low', 'Watt', 'mdi:flash'],
+    'elecprodflowhigh': ['Power Prod High', 'Watt', 'mdi:flash'],
+    'elecusagecntlow': ['Power Use Cnt Low', 'kWh', 'mdi:flash'],
+    'elecusagecnthigh': ['Power Use Cnt High', 'kWh', 'mdi:flash'],
+    'elecprodcntlow': ['Power Prod Cnt Low', 'kWh', 'mdi:flash'],
+    'elecprodcnthigh': ['Power Prod Cnt High', 'kWh', 'mdi:flash'],
+}
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Optional(CONF_PORT, default=10800): cv.positive_int,
+    vol.Required(CONF_RESOURCES, default=[]):
+        vol.All(cv.ensure_list, [vol.In(SENSOR_TYPES)]),
+})
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Setup the Toon smartmeter sensors."""
+    scan_interval = config.get(CONF_SCAN_INTERVAL)
+    host = config.get(CONF_HOST)
+    port = config.get(CONF_PORT)
+
+    try:
+        data = ToonData(host, port)
+    except RunTimeError:
+        _LOGGER.error("Unable to connect fetch Toon data from %s:%s", host, port)
+        return False
+
+    entities = []
+
+    for resource in config[CONF_RESOURCES]:
+        sensor_type = resource.lower()
+
+        if sensor_type not in SENSOR_TYPES:
+            SENSOR_TYPES[sensor_type] = [
+                sensor_type.title(), '', 'mdi:flash']
+
+        entities.append(ToonSmartMeterSensor(data, sensor_type))
+
+    add_entities(entities)
+
+
+# pylint: disable=abstract-method
+class ToonData(object):
+    """Representation of a Toon thermostat."""
+
+    def __init__(self, host, port):
+        """Initialize the thermostat."""
+        self._host = host
+        self._port = port
+        self.data = None
+
+    def get_json_data(self, url):
+        response = urlopen(url)
+        data = response.read().decode("utf-8")
+        return json.loads(data)
+
+    @property
+    def should_poll(self):
+        """Polling needed for thermostat."""
+        return True
+
+    def update(self):
+        """Update the data from the thermostat."""
+        self.data = self.get_json_data(BASE_URL.format(self._host, self._port, '/hdrv_zwave?action=getDevices.json'))
+#        _LOGGER.info("toondata %s", self.data)
+
+ 
+class ToonSmartMeterSensor(Entity):
+    """Representation of a SmartMeter connected to Toon."""
+
+    def __init__(self, data, sensor_type):
+        """Initialize the sensor."""
+        self.data = data
+        self.type = sensor_type
+        self._name = SENSOR_PREFIX + SENSOR_TYPES[sensor_type][0]
+        self._unit = SENSOR_TYPES[sensor_type][1]
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def icon(self):
+        """Icon to use in the frontend, if any."""
+        return SENSOR_TYPES[self.type][2]
+
+    @property
+    def state(self):
+        """Return the state of the sensor. (total/current power consumption/production or total gas used)"""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return self._unit
+
+    def update(self):
+        """Get the latest data and use it to update our sensor state."""
+        self.data.update()
+        json_data = self.data.data
+
+        if self.type == 'gasused':
+            self._state = float(json_data["dev_3.1"]["CurrentGasFlow"])/100
+        elif self.type == 'gasusedcnt':
+            self._state = float(json_data["dev_3.1"]["CurrentGasQuantity"])/1000
+
+        elif self.type == 'elecusageflowlow':
+            self._state = json_data["dev_3.5"]["CurrentElectricityFlow"]
+        elif self.type == 'elecusagecntlow':
+            self._state = float(json_data["dev_3.5"]["CurrentElectricityQuantity"])/1000
+
+        elif self.type == 'elecusageflowhigh':
+            self._state = json_data["dev_3.3"]["CurrentElectricityFlow"]
+        elif self.type == 'elecusagecnthigh':
+            self._state = float(json_data["dev_3.3"]["CurrentElectricityQuantity"])/1000
+
+        elif self.type == 'elecprodflowlow':
+            self._state = json_data["dev_3.6"]["CurrentElectricityFlow"]
+        elif self.type == 'elecprodcntlow':
+            self._state = float(json_data["dev_3.6"]["CurrentElectricityQuantity"])/1000
+
+        elif self.type == 'elecprodflowhigh':
+            self._state = json_data["dev_3.4"]["CurrentElectricityFlow"]
+        elif self.type == 'elecprodcnthigh':
+            self._state = float(json_data["dev_3.4"]["CurrentElectricityQuantity"])/1000
+#        _LOGGER.info("toondata %s", self.data)
+
